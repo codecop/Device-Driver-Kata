a5eaafbea65271e5b2bd55478df1b753a7bbf9d4
Peter Kofler <peter.kofler@code-cop.org>
2019-05-09 00:24:38 +0200

    Remove duplication.

--- Go/driver.go
+++ Go/driver.go
@@ -5,6 +5,12 @@ import "fmt"
 const control uint32 = 0x0
 
 const programCommand byte = 0x40
+const clearCommand byte = 0xFF
+
+const hardwareErrorBit byte = 0x08
+const internalErrorBit byte = 0x10
+const protectionErrorBit byte = 0x20
+const errorBits byte = hardwareErrorBit | internalErrorBit | protectionErrorBit
 const readyBit byte = 0x80
 
 // DeviceDriver is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
@@ -23,10 +29,12 @@ func (driver DeviceDriver) Write(address uint32, data byte) error {
 	var status byte
 	for status&readyBit == 0 {
 		status = driver.device.Read(control)
+		// TODO timeout
 	}
-	if status&0x8 != 0 {
-		driver.device.Write(control, 0xFF)
-		return DeviceError{"Hardware Error", address, data}
+
+	if status&errorBits != 0 {
+		driver.device.Write(control, clearCommand)
+		return DeviceError{status & errorBits, address, data}
 	}
 
 	driver.device.Read(address)
@@ -34,12 +42,26 @@ func (driver DeviceDriver) Write(address uint32, data byte) error {
 	return nil
 }
 
+// DeviceError is the error caused by hardware errors.
 type DeviceError struct {
-	cause   string
-	address uint32
-	data    byte
+	errorBits byte
+	address   uint32
+	data      byte
 }
 
 func (error DeviceError) Error() string {
-	return fmt.Sprintf("%s at 0x%X", error.cause, error.address)
+	return fmt.Sprintf("%s at 0x%X", error.cause(), error.address)
+}
+
+func (error DeviceError) cause() string {
+	if error.errorBits&hardwareErrorBit != 0 {
+		return "Hardware Error"
+	}
+	if error.errorBits&internalErrorBit != 0 {
+		return "Internal Error"
+	}
+	if error.errorBits&protectionErrorBit != 0 {
+		return "Protected Block Error"
+	}
+	return "Unknown Error"
 }
