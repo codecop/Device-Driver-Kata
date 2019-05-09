58a4a86a1b5f8b7648bb8c54f48b687cabda3b67
Peter Kofler <peter.kofler@code-cop.org>
2019-05-09 23:13:37 +0200

    Extract hardwareStatus type with query methods.

--- Go/driver.go
+++ Go/driver.go
@@ -2,16 +2,42 @@ package codekata
 
 import "fmt"
 
-const control uint32 = 0x0
+const controlAddress uint32 = 0x0
 
 const programCommand byte = 0x40
 const clearCommand byte = 0xFF
 
-const hardwareErrorBit byte = 0x08
-const internalErrorBit byte = 0x10
-const protectionErrorBit byte = 0x20
-const errorBits byte = hardwareErrorBit | internalErrorBit | protectionErrorBit
-const readyBit byte = 0x80
+// hardwareStatus is the returned bitmask
+type hardwareStatus byte
+
+const readyBit hardwareStatus = 0x80
+const hardwareErrorBit hardwareStatus = 0x08
+const internalErrorBit hardwareStatus = 0x10
+const protectionErrorBit hardwareStatus = 0x20
+
+func (status hardwareStatus) notReady() bool {
+	return status&readyBit == 0
+}
+
+func (status hardwareStatus) isSuccess() bool {
+	return !(status.isHardwareError() || status.isInternalError() || status.isProtectionError())
+}
+
+func (status hardwareStatus) notRecoverableError() bool {
+	return status.isHardwareError() || status.isProtectionError()
+}
+
+func (status hardwareStatus) isHardwareError() bool {
+	return status&hardwareErrorBit != 0
+}
+
+func (status hardwareStatus) isInternalError() bool {
+	return status&internalErrorBit != 0
+}
+
+func (status hardwareStatus) isProtectionError() bool {
+	return status&protectionErrorBit != 0
+}
 
 const retries = 3
 
@@ -25,59 +51,65 @@ func (driver DeviceDriver) Read(address uint32) (byte, error) {
 }
 
 func (driver DeviceDriver) Write(address uint32, data byte) error {
-	var status byte // TODO status object with queries for kind of errors, hide bit masking...
+	var status hardwareStatus
 
 	for try := 0; try <= retries; try++ {
 		driver.writeData(address, data)
+
 		status = driver.waitReady()
-		if status&errorBits == 0 {
+		if status.isSuccess() {
 			return nil
 		}
+
 		driver.reset()
-		if status&internalErrorBit == 0 {
+		if status.notRecoverableError() {
 			break
 		}
 	}
 
-	return DeviceError{address, data, status}
+	return deviceError{address, data, status}
 }
 
 func (driver DeviceDriver) writeData(address uint32, data byte) {
-	driver.device.Write(control, programCommand)
+	driver.writeControl(programCommand)
 	driver.device.Write(address, data)
 }
 
-func (driver DeviceDriver) waitReady() byte {
-	var status byte
-	for status&readyBit == 0 {
-		status = driver.device.Read(control)
+func (driver DeviceDriver) writeControl(data byte) {
+	driver.device.Write(controlAddress, data)
+}
+
+func (driver DeviceDriver) waitReady() hardwareStatus {
+	var status hardwareStatus
+	for status.notReady() {
+		status = hardwareStatus(driver.device.Read(controlAddress))
 	}
 	return status
 }
 
 func (driver DeviceDriver) reset() {
-	driver.device.Write(control, clearCommand)
+	driver.writeControl(clearCommand)
 }
 
-// DeviceError is the error caused by hardware errors.
-type DeviceError struct {
+// deviceError is the error caused by hardware errors.
+type deviceError struct {
 	address   uint32
 	data      byte
-	errorBits byte
+	errorBits hardwareStatus
 }
 
-func (e DeviceError) Error() string {
+func (e deviceError) Error() string {
 	return fmt.Sprintf("%s at 0x%X", e.cause(), e.address)
 }
 
-func (e DeviceError) cause() string {
-	if e.errorBits&hardwareErrorBit != 0 {
+func (e deviceError) cause() string {
+	if e.errorBits.isHardwareError() {
 		return "Hardware Error"
 	}
-	if e.errorBits&internalErrorBit != 0 {
+	if e.errorBits.isInternalError() {
 		return "Internal Error"
 	}
-	if e.errorBits&protectionErrorBit != 0 {
+	if e.errorBits.isProtectionError() {
 		return "Protected Block Error"
 	}
 	return "Unknown Error"
